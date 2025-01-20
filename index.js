df7634974606cf6906293425c492da3aaaa5043d
henrikH711 <hegyihenrik72@gmail.com>
2025-01-20 21:49:03 +0100

    js-edit5

--- index.js
+++ index.js
@@ -73,16 +73,9 @@ document.getElementById('file-input').addEventListener('change', (event) => {
     }
 });
 
-// Resize handler
+// Resize 
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
-// Animation loop
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
